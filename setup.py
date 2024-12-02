57141572c0990dddb8bfbba2921f733d710f47e2
1078824714 <131585297+1078824714@users.noreply.github.com>
2024-12-02 20:33:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     version='0.1',
     packages=find_packages(),
     description='A Python library for calculating the similarity of cpp codes',
-    url='https://github.com/1078824714/code_similarity_cpp',  
     author='1078824714',
+    author_email='1078824714@qq.com',
+    url='https://github.com/1078824714/code_similarity_cpp',  
     install_requires=['tree_sitter_cpp','tree_sitter'],  
 )
